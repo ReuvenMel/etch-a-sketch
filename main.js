cecb05311c555a7ac78e529f252eff010a4f48a7
ReuvenMel <154438638+ReuvenMel@users.noreply.github.com>
2023-12-30 12:28:18 +0000

    added color picker, style fixes

--- main.js
+++ main.js
@@ -2,52 +2,61 @@ document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
     let popup = document.getElementById("popup");
     popup.onclick = selectSize;
-});
-
-let currentColor = 'red'; // Initial color
-
-function changeColor(color) {
+  });
+  
+  let currentColor = 'red'; // Initial color
+  
+  function changeColor(color) {
     currentColor = color;
-}
-
-function selectSize() {
+  }
+  
+  function selectSize() {
     let size = prompt("Size?");
-    
+  
     if (isValidSize(size)) {
         createBoard(parseInt(size));
     } else {
         alert("Please enter a valid number between 1 and 128");
     }
-}
-
-function isValidSize(size) {
+  }
+  
+  function isValidSize(size) {
     return !isNaN(size) && parseInt(size) >= 1 && parseInt(size) <= 128;
-}
-
-function colorDiv(element) {
+  }
+  
+  function colorDiv(element) {
     element.style.backgroundColor = currentColor;
-}
-
-function createBoard(size) {
+  }
+  
+  function createBoard(size) {
     let board = document.querySelector(".board");
-    
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
+  
+    board.style.gridTemplateColumns = repeat(${size}, 1fr);
+    board.style.gridTemplateRows = repeat(${size}, 1fr);
+  
     for (let i = 0; i < size * size; i++) {
         let div = document.createElement("div");
         div.classList.add("divGrid");
         board.insertAdjacentElement('beforeend', div);
-        div.addEventListener("mousedown", function() {
+        div.addEventListener("mouseover", function() {
             colorDiv(div);
         });
+  
+        let selectedSize = document.getElementById('selectedSize');
+  
+        selectedSize.innerHTML = "Selected size is: " + size;
+  
+  
     }
-}
-
-function resetBoard() {
+  }
+  
+  function resetBoard() {
     let divGridElements = document.querySelectorAll(".divGrid");
-
+  
     divGridElements.forEach(function(element) {
         element.style.backgroundColor = '';
     });
-}
+  
+  
+  
+  }
